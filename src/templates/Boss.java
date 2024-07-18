b266e4db4b303e80ceafe2304e32beae83198c8a
Emir Karamoldo <emirkaramoldo@proton.me>
2024-07-18 17:11:19 +0600

    Создан класс Босса

---
+++ src/templates/Boss.java
@@ -0,0 +1,32 @@
+package templates;
+
+public class Boss {
+    private int health;
+    private int damage;
+    private String defenceType;
+
+    public int getHealth(){return this.health;}
+    public int getDamage(){return this.damage;}
+    public String getDefenceType(){return this.defenceType;}
+    public void setHealth(int newHealth){
+        if (newHealth > 0) {
+            this.health = newHealth;
+        } else {
+            System.out.println("Can't be zero or lower");
+        }
+    }
+    public void setDamage(int newDamage){
+        if (newDamage > 0) {
+            this.damage = newDamage;
+        } else {
+            System.out.println("Can't be zero or lower");
+        }
+    }
+    public void setDefenceType(String newDefenceType){
+        if (newDefenceType != null) {
+            this.defenceType = newDefenceType;
+        } else {
+            System.out.println("Can't be empty");
+        }
+    }
+}
